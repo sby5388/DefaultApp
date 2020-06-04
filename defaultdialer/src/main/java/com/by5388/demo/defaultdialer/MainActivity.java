46f9e0fb9efeceb643fa74eb6fa49026704504c1
shenbingyan <shenby@server03>
2020-06-04 10:57:39 +0800

    init default dialer

---
+++ defaultdialer/src/main/java/com/by5388/demo/defaultdialer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.by5388.demo.defaultdialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 默认拨号器
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
