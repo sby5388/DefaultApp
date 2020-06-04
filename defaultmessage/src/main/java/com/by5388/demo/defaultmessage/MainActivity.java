bef6356e15571c95be28cadf29ba69071440f1e6
shenbingyan <shenby@server03>
2020-06-04 10:58:44 +0800

    add default message no finish

---
+++ defaultmessage/src/main/java/com/by5388/demo/defaultmessage/MainActivity.java
@@ -0,0 +1,17 @@
+package com.by5388.demo.defaultmessage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 默认短信应用
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
